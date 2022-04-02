199adaf488ee365bcfe6dd6d768bbdaa0679a6f3
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-04-02 15:01:39 -0700

    wrote sql tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,24 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS posts CASCADE;
+
+CREATE TABLE users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  username TEXT NOT NULL,
+  email TEXT NOT NULL,
+  avatar_url TEXT
+);
+
+CREATE TABLE posts (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title TEXT NOT NULL,
+  test VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO 
+  posts (post)
+VALUES 
+  ('Oh my glob, I love to post.'), 
+  ('Butts.');
